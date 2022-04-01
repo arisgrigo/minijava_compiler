6acbaec51e280227c16de6e23fd70e048e33e359
Aris G <46575802+arisgrigo@users.noreply.github.com>
2022-04-01 13:32:22 +0300

    Add files via upload

---
+++ tests/extends.java
@@ -0,0 +1,22 @@
+class MoreThan4{
+    public static void main(String[] a){
+    }
+}
+
+class A {
+
+     int i; 
+
+} 
+
+class B extends A {
+
+     int y; 
+
+}
+
+class C extends A {
+
+     int z;
+
+}
